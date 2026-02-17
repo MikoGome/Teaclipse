c0cc639cd056aea12e88dedfe8c58689680dacba
MikoGome <lunalescia@gmail.com>
2026-02-16 21:53:35 -0800

    fixed menu layout

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -6,8 +6,8 @@ export const Menu = () => (
   <>
     <Header className="pt-12"/>
     <figure className="flex flex-col md:flex-row">
-      <img src={menu_page_1} className="w-full md:flex-1" />
-      <img src={menu_page_2} className="w-full md:flex-1" />
+      <img src={menu_page_1} className="w-full md:flex-1 md:min-w-0" />
+      <img src={menu_page_2} className="w-full md:flex-1 md:min-w-0" />
     </figure>
   </>
 );
