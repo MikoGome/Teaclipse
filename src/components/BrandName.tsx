54d6c819780acb44d6fcd1bcfbcb02f1a933c518
MikoGome <lunalescia@gmail.com>
2026-02-19 16:15:30 -0800

    adjusted drop shadow

--- src/components/BrandName.tsx
+++ src/components/BrandName.tsx
@@ -8,7 +8,7 @@ type BrandNameProps = {
 
 export const BrandName = (props: BrandNameProps) => (
   <h1
-    className={`text-black drop-shadow drop-shadow-lightorange text-6xl lg:text-8xl -rotate-6 font-petit ${props.className ?? ''}`}
+    className={`text-black drop-shadow-[0_0px_6px_rgba(0,0,0,1)] drop-shadow-lightorange/85 text-6xl lg:text-8xl -rotate-6 font-petit ${props.className ?? ''}`}
   >
     <HashLink smooth to="/#top">{NAME.toLowerCase()}</HashLink>
   </h1>
